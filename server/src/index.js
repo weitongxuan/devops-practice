0919a2f28354d50cbe4e4a58497c70aaca437d47
weitongxuan <hellosimon27@gmail.com>
2022-07-04 02:16:01 +0800

    aaaa

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get("/:name", (req, res) => {
+app.get("/:name1", (req, res) => {
   res.send(`hello ${req.params.name}`);
 });
 
